7588446aef02d2e855ac6f87e5b5de193259561b
Jinyoung Jang <jyjang@uengine.org>
2022-11-28 02:01:04 +0900

    commit

--- src/main/java/com/eventstorming/api/Controller.java
+++ src/main/java/com/eventstorming/api/Controller.java
@@ -1,12 +1,3 @@
-
-
-
-
-
-
-
-
-
 forEach: Aggregate
 fileName: {{namePascalCase}}Controller.java
 path: {{boundedContext.name}}/{{{options.packagePath}}}/api
@@ -16,6 +7,8 @@ package {{options.package}}.api;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.queryhandling.QueryGateway;
 
+import org.axonframework.eventsourcing.eventstore.EventStore;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.axonframework.eventsourcing.eventstore.EventStore;
 
 import org.springframework.beans.BeanUtils;
@@ -33,6 +28,7 @@ import org.springframework.hateoas.Link;
 import org.springframework.http.HttpEntity;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.ArrayList;
 
 
 import {{options.package}}.aggregate.*;
@@ -98,12 +94,15 @@ public class {{ namePascalCase }}Controller {
   EventStore eventStore;
 
   @GetMapping(value="/{{ namePlural }}/{id}/events")
-  public String getEvents(@PathVariable("id") {{aggregateRoot.keyFieldDescriptor.className}} id){
-      eventStore.readEvents(id).asStream().forEach(System.out::println);
+  public ResponseEntity getEvents(@PathVariable("id") {{aggregateRoot.keyFieldDescriptor.className}} id){
+      ArrayList resources = new ArrayList<{{namePascalCase}}Aggregate>(); 
+      eventStore.readEvents(id).asStream().forEach(resources::add);
 
-      return "ok";
-            
-  }
+      CollectionModel<VacationAggregate> model = CollectionModel.of(resources);
+                
+      return new ResponseEntity<>(model, HttpStatus.OK);
+  } 
+
 
 }
 //>>> Clean Arch / Inbound Adaptor
