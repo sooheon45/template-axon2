58260c497da11bc30f223be2d3ad46cce56adea5
Jinyoung Jang <jyjang@uengine.org>
2022-11-27 17:37:38 +0900

    commit

--- src/main/java/com/eventstorming/query/ReadModelRepository.java
+++ src/main/java/com/eventstorming/query/ReadModelRepository.java
@@ -2,6 +2,7 @@
 
 
 
+
 forEach: View
 fileName: {{namePascalCase}}Repository.java
 path: {{boundedContext.name}}/{{{options.packagePath}}}/query
@@ -10,10 +11,30 @@ mergeType: template
 package {{options.package}}.query;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "{{namePlural}}_", collectionResourceRel = "{{namePlural}}_")
 public interface {{namePascalCase}}Repository extends JpaRepository<{{namePascalCase}}, Long> {
+
+    @Override
+    @RestResource(exported = false)
+    void delete(OrderStatus entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long id);
+
+    @Override
+    @RestResource(exported = false)
+     <S extends OrderStatus> S save(S entity);
+
 
     {{#setFindBy updateRules namePascalCase}}{{/setFindBy}}
 
