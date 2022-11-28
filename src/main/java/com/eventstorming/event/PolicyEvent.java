c4d748bcbe7cbc9559b2c5cc21aa33264484e7a9
sooheon45 <54785805+sooheon45@users.noreply.github.com>
2022-11-28 18:05:23 +0900

    Update PolicyEvent.java

--- src/main/java/com/eventstorming/event/PolicyEvent.java
+++ src/main/java/com/eventstorming/event/PolicyEvent.java
@@ -1,7 +1,3 @@
-
-
-
-
 forEach: RelationEventInfo
 fileName: {{eventValue.namePascalCase}}Event.java
 path: {{boundedContext.name}}/{{{options.packagePath}}}/event
@@ -16,6 +12,21 @@ import lombok.*;
 public class {{eventValue.namePascalCase}}Event {
 
 {{#eventValue.fieldDescriptors}}
-    private {{className}} {{nameCamelCase}};
+    private {{safeTypeOf className}} {{nameCamelCase}};
 {{/eventValue.fieldDescriptors}}
 }
+
+<function>
+    window.$HandleBars.registerHelper('safeTypeOf', function (className) {
+        if(className.endsWith("String") || className.endsWith("Integer") || className.endsWith("Long") || className.endsWith("Double") || className.endsWith("Float")
+            || className.endsWith("Boolean") || className.endsWith("Date")){
+            return className;
+        } else {
+            return "Object";
+        }
+    
+        
+    })
+
+
+</function>
